4f51276866a671b9f9969d1d6afdc3fbad346090
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-13 21:37:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "bucket_name" {default = "demo0000000"}
-variable "artifact_bucket" {}
+variable "artifact_bucket" {default = "prasholademo01"}
 variable "instance_type" { default = "t2.micro" }
 variable "ami_id" { default = "ami-04f7a54071e74f488" }
 variable "key_name" {default = "first"}
