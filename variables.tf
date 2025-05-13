a86f9d6767bf83983af264c8bb6d640d5cf05f9f
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-13 19:40:01 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "bucket_name" {}
+variable "artifact_bucket" {}
+variable "instance_type" { default = "t2.micro" }
+variable "ami_id" { default = "ami-0c55b159cbfafe1f0" }
+variable "key_name" {}
+variable "end_user_arn" {}
