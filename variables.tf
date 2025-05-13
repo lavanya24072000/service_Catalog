0ab295b2297aff8aeba8b1e5260844f43aade654
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-13 20:04:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "bucket_name" {}
 variable "artifact_bucket" {}
 variable "instance_type" { default = "t2.micro" }
-variable "ami_id" { default = "ami-0c55b159cbfafe1f0" }
+variable "ami_id" { default = "ami-04f7a54071e74f488" }
 variable "key_name" {}
 variable "end_user_arn" {}
