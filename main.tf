23ecd31a6b693ecaf3fdab855cdfbd9bfe9f851b
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-13 19:39:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,49 @@
+module "s3" {
+  source      = "./modules/s3"
+  bucket_name = var.bucket_name
+}
+ 
+module "ec2" {
+  source        = "./modules/ec2"
+  instance_type = var.instance_type
+  ami_id        = var.ami_id
+  key_name      = var.key_name
+}
+ 
+resource "aws_s3_bucket_object" "tf_zip_upload" {
+  bucket = var.artifact_bucket
+key = "terraform.zip"
+source = "terraform.zip"
+etag = filemd5("terraform.zip")
+}
+ 
+resource "aws_servicecatalog_portfolio" "demo_portfolio" {
+  name          = "DevOps Demo Portfolio"
+  description   = "Contains EC2 and S3 Terraform deployment"
+  provider_name = "DevOps Team"
+}
+ 
+resource "aws_servicecatalog_product" "demo_product" {
+  name  = "EC2 and S3 Terraform Product"
+  owner = "DevOps Team"
+  type  = "TERRAFORM_OPEN_SOURCE"
+ 
+  provisioning_artifact_parameters {
+    name        = "v1"
+    description = "Initial version"
+    type        = "TERRAFORM_OPEN_SOURCE"
+    info = {
+LoadTemplateFromURL = "https://${var.artifact_bucket}.s3.amazonaws.com/terraform.zip"
+    }
+  }
+}
+ 
+resource "aws_servicecatalog_portfolio_product_association" "assoc" {
+portfolio_id = aws_servicecatalog_portfolio.demo_portfolio.id
+product_id = aws_servicecatalog_product.demo_product.id
+}
+ 
+resource "aws_servicecatalog_portfolio_user_association" "user_assoc" {
+portfolio_id = aws_servicecatalog_portfolio.demo_portfolio.id
+  user         = var.end_user_arn
+}
